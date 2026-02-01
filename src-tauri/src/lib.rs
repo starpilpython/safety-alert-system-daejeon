71c584c0f33ed529cbe3acc044ce341395a06832
starpilpython <167661158+starpilpython@users.noreply.github.com>
2026-02-01 22:13:46 +0900

    Delete src-tauri directory

--- src-tauri/src/lib.rs
+++
@@ -1,8 +0,0 @@
-#[cfg_attr(mobile, tauri::mobile_entry_point)]
-pub fn run() {
-    tauri::Builder::default()
-        .plugin(tauri_plugin_dialog::init()) // 이 줄 반드시 추가
-        .plugin(tauri_plugin_opener::init())
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
-}
